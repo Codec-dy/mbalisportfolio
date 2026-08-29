c602bd166cbb2562c2e8767761f95ac0eab4f6eb
Codec-dy <70746104+Codec-dy@users.noreply.github.com>
2026-08-28 22:08:20 -0500

    Fix import path for PortfolioContext

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
-import { PortfolioContext } from '../context/portfolioContext'
+import { PortfolioContext } from '../context/PortfolioContext'
 import ProPageTop from '../components/ProPageTop'
 import Button from '../components/Button'
 import { getData } from '../components/Utils/crud'
